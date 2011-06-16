70b928bf88c95a54ceb79ecb1bdc07f4711ea28d
Danilo Sousa <eu@danilosousa.net>
2011-06-16 18:41:57 -0300

    Mongoose models;

---
+++ faker/lib/models.js
@@ -0,0 +1,65 @@
+/**
+ * Module dependencies
+ */
+
+var
+	Match, Play, Team, Player,
+	mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	ObjectId = Schema.ObjectId;
+
+/**
+ * Connect to database
+ */
+
+mongoose.connect('mongodb://localhost/minute');
+
+/**
+ * Players
+ */
+
+Player = new Schema({
+	number: Number,
+	name: String
+});
+
+/**
+ * Teams
+ */
+
+Team = new Schema({
+	name: String,
+	acronym: String,
+	score: { type: Number, default: 0 },
+	rooster: [ Player ],
+	substitutes: [ Player ]
+});
+
+/**
+ * Plays
+ */
+
+Play = new Schema({
+	time: Number,
+	type: String,
+	text: String
+});
+
+/**
+ * Matches
+ */
+
+Match = new Schema({
+	time: { type: Number, default: 0 },
+	plays: [ Play ],
+	teams: [ Team ]
+});
+
+/**
+ * Exports all models
+ */
+
+exports.Plays = mongoose.model('plays', Play);
+exports.Players = mongoose.model('players', Player);
+exports.Teams = mongoose.model('teams', Team);
+exports.Matches = mongoose.model('matches', Match);
