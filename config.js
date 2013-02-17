fa64f80406680b67698c051410e2e76780b16ab4
Danilo Sousa <code@danilosousa.net>
2013-02-16 23:14:01 -0200

    put back config.js
    this will be moved to foreman's .env file for heroku deployment

---
+++ config.js
@@ -0,0 +1,10 @@
+exports = module.exports = {
+  'development': {
+    'port': 3000,
+    'mongo': 'mongodb://localhost/minute',
+    'redis': {
+      'port': 6379,
+      'host': 'localhost'
+    }
+  }
+};
